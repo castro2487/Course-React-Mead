d88f7350bf8dc3d6b273ae42be9040c4e940fbc3
CASTRO CARDENAS Roger Omar <CastroR@sancristobal.com.ar>
2017-12-21 18:07:43 -0300

    boberas2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,87 @@
-"use strict";
+'use strict';
 
 console.log("el app js esta corriendo");
 
+var app = {
+  titulo: 'App Indecision',
+  subtitulo: 'Esta es una info X'
+};
+
 var template = React.createElement(
-  "h1",
+  'div',
   null,
-  " App Indecision "
+  React.createElement(
+    'h1',
+    null,
+    ' ',
+    app.titulo,
+    ' '
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' ',
+    app.subtitulo,
+    ' '
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 2'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 3'
+    )
+  )
+);
+
+var usuario = {
+  nombre: 'Omar',
+  edad: 30,
+  ubicacion: 'Buenos Aires'
+};
+
+function encontrarUbicacion(ubicacion) {
+  if (ubicacion) {
+    return ubicacion;
+  } else {
+    return 'desconocido';
+  }
+}
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    ' ',
+    usuario.nombre,
+    ' '
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Edad: ',
+    usuario.edad
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Ubicacion: ',
+    encontrarUbicacion(usuario.ubicacion)
+  )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,44 @@
 console.log("el app js esta corriendo");
 
-var template = <h1> App Indecision </h1>;
+var app = {
+  titulo: 'App Indecision',
+  subtitulo: 'Esta es una info X'
+}
+
+var template =( 
+<div>
+   <h1> {app.titulo} </h1>
+   <p> {app.subtitulo} </p>
+   <ol>
+     <li>Item 1</li>
+     <li>Item 2</li>
+     <li>Item 3</li>
+   </ol>
+</div>
+);
+
+var usuario = {
+  nombre: 'Omar',
+  edad: 30,
+  ubicacion: 'Buenos Aires'
+};
+
+
+function encontrarUbicacion(ubicacion){
+  if (ubicacion) {
+    return ubicacion;
+  } else {
+    return 'desconocido';
+  }
+}
+
+var template2 = (
+  <div>
+    <h1> {usuario.nombre} </h1>
+    <p>Edad: {usuario.edad}</p>
+    <p>Ubicacion: {encontrarUbicacion(usuario.ubicacion)}</p>
+  </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
